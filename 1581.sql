0c5ac9226987034a62356530371ccbb507e94bc5
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-08-10 18:46:43 +0530

    Update 1581.sql

--- 1581.sql
+++ 1581.sql
@@ -1,3 +1,10 @@
+-- 1
+SELECT customer_id, COUNT(*) as count_no_trans
+FROM Visits 
+WHERE visit_id NOT IN (SELECT visit_id FROM Transactions)
+GROUP BY customer_id;
+
+-- 2
 SELECT v.customer_id, COUNT(v.visit_id) as count_no_trans
 FROM Visits v
 LEFT OUTER JOIN Transactions t
