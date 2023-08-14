a5fe8e19aeb98fe45bc4f7e80687aa866990273f
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-08-14 20:53:29 +0530

    Update 610.py

--- 610.py
+++ 610.py
@@ -1,2 +1,7 @@
 SELECT x,y,z,if(x+y>z and x+z>y and y+z>x,'Yes','No') as triangle
 FROM Triangle;
+
+
+SELECT x,y,z,
+CASE WHEN x+y>z AND x+z>y AND y+z>x THEN 'Yes' ELSE 'No' END as triangle
+FROM Triangle;
